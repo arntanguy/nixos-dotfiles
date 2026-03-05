f5ad518be3b22da7ecd9a837cbdc1e3f88006d7d
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-05 19:00:02 +0100

    feat(keyboard): allow to disable home-row mods

--- modules/keyboard/default.nix
+++ modules/keyboard/default.nix
@@ -6,32 +6,36 @@
     };
   };
 
-  config = lib.mkIf config.modules.keyboard-mods.enable {
+  config = lib.mkMerge [
+  {
     services.xserver.xkb = {
       layout = "${globals.xkb.layout}";
       variant = "";
       options = "${globals.xkb.options}";
-     };
-
-    environment.systemPackages = with pkgs; [
-      kmonad
-    ];
-
-    # home-row mods
-    # see https://precondition.github.io/home-row-mods
-    services.kmonad = {
-     enable = true;
-     keyboards = {
-       myKMonadOutput = {
-         defcfg = {
-           enable = true;
-           fallthrough = true;
-         };
-         # XXX how to configure per-host?
-         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-         config = builtins.readFile ./kmonad/config.kbd;
-       };
-     };
     };
-  };
+  }
+  (lib.mkIf config.modules.keyboard-mods.enable {
+
+   environment.systemPackages = with pkgs; [
+   kmonad
+   ];
+
+# home-row mods
+# see https://precondition.github.io/home-row-mods
+   services.kmonad = {
+   enable = true;
+   keyboards = {
+   myKMonadOutput = {
+   defcfg = {
+   enable = true;
+   fallthrough = true;
+   };
+# XXX how to configure per-host?
+   device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+   config = builtins.readFile ./kmonad/config.kbd;
+   };
+   };
+   };
+  })
+  ];
 }
