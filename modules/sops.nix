9ae45cb3cd73b0428382e64641b1345565fdeb12
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-03-16 13:27:27 +0100

    feat(sops): Add access to github private repositories in nix sandbox

--- modules/sops.nix
+++ modules/sops.nix
@@ -17,8 +17,23 @@
       # secrets are available as root in /run/secrets/<path> by default
       secrets = {
           "data/networking/wifi/LIRMM"= { };
+          # XXX: Needed to enable nix build to fetch private repositories
+          "data/github/NIX_GH_REPO_TOKEN"= { };
       };
     };
+
+    # Create a /etc/nix/netrc file with a github token to access private repositories
+    # from within nix sandbox
+    sops.templates.netrc = {
+      content = ''
+        machine github.com
+            password ${config.sops.placeholder."data/github/NIX_GH_REPO_TOKEN"}
+      '';
+      mode = "0400";
+      path = "/etc/nix/netrc";
+    };
+    # allow sandboxed builds access to private repositories
+    nix.settings.netrc-file = "/etc/nix/netrc";
 
   };
 }
